6144126d41398bca50751e2b81e56972a3db9e3e
Aditya Dubey <hmmho705@gmail.com>
2026-07-29 23:20:25 +0530

    added progress bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import NextTopLoader from "nextjs-toploader";
 import "./globals.css";
+
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +30,14 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <NextTopLoader
+          color="#ffffff"
+          height={3}
+          showSpinner={false}
+        />
+        {children}
+      </body>
     </html>
   );
 }
