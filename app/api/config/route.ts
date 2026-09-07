9d6a082a6d7cb4e0af64dbc4c69b2e96db63d5ba
Aditya Dubey <hmmho705@gmail.com>
2026-09-08 00:16:43 +0530

    added api for the config update

--- app/api/config/route.ts
+++ app/api/config/route.ts
@@ -1,8 +1,42 @@
-export async function PATCH(req: Request){
-    const request = await req.json()
+import { prisma } from "@/lib/prisma";
 
-    console.log(request)
-    return Response.json({
-        success: true
-    });
+export async function PATCH(req: Request){
+    let request;
+    
+    try{
+        request = await req.json();
+        console.log(request)
+    }catch{
+       return Response.json({
+            success: false,
+            error: "Invalid Request"
+        }, { status: 400 });
+    }
+
+    if(!request || typeof request.fromNameEnabled !== "boolean"){
+        return Response.json({
+            success: false,
+            error: "Invalid Request"
+        });
+    }
+
+    try{
+        const config = await prisma.config.update({
+            where: {id: "global"},
+            data: {
+                fromNameEnabled: request.fromNameEnabled
+            }
+        })
+
+        return Response.json({
+            success: true,
+            config
+        });
+    }catch(err){
+        console.log(err);
+        return Response.json({
+            success: false,
+            error: "Something went wrong."
+        });
+    }
 }
